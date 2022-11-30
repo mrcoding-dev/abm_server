dcec895eaa47b2ba37173052e2cd2dc71abfe6f8
gabriel <gbordesc@uft.edu>
2022-11-30 09:42:38 -0300

    subir

---
+++ main.py
@@ -0,0 +1,19 @@
+import utilx
+
+print(utilx.serial_ports())
+import serial
+
+
+#serial1 = serial.Serial(port='/dev/tty.R2D2', baudrate=9600, timeout=1, write_timeout=1)
+#serial1 = serial.Serial(port='/dev/tty.ArduinoMasterBluetooh', baudrate=9600, timeout=1, write_timeout=1)
+
+
+#mystring = "1"
+#convert to byte
+
+
+#while True:
+#    serial1.write(bytes(mystring, 'utf-8'))
+#    serial1.readline()
+#    print("Mensaje enviado")
+
