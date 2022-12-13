c5c4684ee7e407fdec45edfbdc0a226ac065b918
Ssebv <sallendec@uft.edu>
2022-12-13 16:31:11 -0300

    andando

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,33 +1,221 @@
-#include <SoftwareSerial.h>   // Incluimos la librería  SoftwareSerial  
-SoftwareSerial BT(10,11);    // Definimos los pines RX y TX del Arduino conectados al Bluetooth
- 
-void setup()
-{
-  BT.begin(9600);       // Inicializamos el puerto serie BT (Para Modo AT 2)
-  Serial.begin(9600);   // Inicializamos  el puerto serie  
+#include <Adafruit_MotorShield.h>
+#include <SoftwareSerial.h>   
+
+SoftwareSerial BT(10,11);    //6 7  10 11 (rx, tx)
+
+unsigned char *buffer;
+String mensaje = "";
+
+// Create the motor shield object with the default I2C address
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+// Or, create it with a different I2C address (say for stacking)
+// Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x61);
+
+// Select which 'port' M1, M2, M3 or M4. In this case, M1
+Adafruit_DCMotor *myMotor1 = AFMS.getMotor(1);
+Adafruit_DCMotor *myMotor2 = AFMS.getMotor(2);
+// You can also make another motor on port M2
+//Adafruit_DCMotor *myOtherMotor = AFMS.getMotor(2);
+
+#define LEYENDO 1
+#define LEIDO 2
+#define ESPERANDO 0
+
+#define CARGA 1
+#define ADELANTE 2
+#define DESCARGA 3
+#define ATRAS 4
+
+#define DESACTIVADO 0
+#define ACTIVADO 1
+// valor entre 1 y 5 para el mensaje que recibo
+
+// Mensaje para matar al robot
+#define Desactivar 'f'
+// Mensaje grua 1
+#define inGrua1 'a'
+#define outGrua1 'c'
+//mensaje grua2
+#define inGrua2 'r'
+#define outGrua2 'd'
+// Sensores
+//Adelante
+#define echoPin 12 
+#define trigPin 13 
+
+long duration1;
+int distance1;
+//Atras
+#define echoPin2 2 
+#define trigPin2 3 
+
+long duration2;
+int distance2;
+
+int estado;
+int desviado;
+void setup() {
+  
+  BT.begin(9600);  //AT1:9600 AT2:38400    
+  buffer = malloc(1000);
+  Serial.begin(9600);           // set up Serial library at 9600 bps
+
+  if (!AFMS.begin()) {         // create with the default frequency 1.6KHz
+  // if (!AFMS.begin(1000)) {  // OR with a different frequency, say 1KHz
+    Serial.println("Could not find Motor Shield. Check wiring.");
+    while (1);
+  }
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
+  pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
+  pinMode(trigPin2, OUTPUT); // Sets the trigPin as an OUTPUT
+  pinMode(echoPin2, INPUT); // Sets the echoPin as an INPUT
+  
+  // Set the speed to start, from 0 (off) to 255 (max speed)
+  estado = CARGA;
+  desviado = DESACTIVADO;
+  myMotor1->setSpeed(70);
+  myMotor1->run(FORWARD);
+  myMotor1->run(RELEASE);
+  myMotor2->setSpeed(70);
+  myMotor2->run(FORWARD);
+  myMotor2->run(RELEASE);
+  //BT.write(outGrua1);
 }
- 
 
-void hacerAlgo(){
-  //Esta funcion es lo que debe hacer el arduino, de resto es comunicacion
-}
+int st_read = ESPERANDO;
+int size = 0;
 
 
-void loop()
-{
-  if(BT.available())    // Aca si llega un dato se imprime
-  {
+void recibir(){
+  unsigned char c;
+  if(BT.available())   
+  { 
+
     //Serial.write(BT.read());
+    c = BT.read();
+    Serial.println(char(c));
+    if (st_read == LEYENDO && c == '}')
+    {
+      size = size + 1;
+      buffer[size] = c;      
+      for (int i=0; i<size+1;i++){
+          mensaje =  mensaje + String(char(buffer[i]));
+          
+      }
+      st_read = LEIDO;
+    }
+    if (st_read == ESPERANDO && c == '{'){
+      size = 0;
+      st_read = LEYENDO;
+      buffer[size] = c;
+    }
+    if (st_read == LEYENDO && c != '{' && c != '}'){
+      size = size + 1;
+      buffer[size] = c;
+    }
+  }  
+}
 
-     Serial.print("Recibi: ");
-     Serial.print(BT.readString());
-    Serial.print("\n");
+int mensaje_disp(){
+    if (st_read == LEIDO){
+      return 1;
+    }
+    return 0;
+}
+
+String leerMensaje(){
+  String aux = mensaje;
+    mensaje = "";
+    st_read = ESPERANDO;
+    return aux;
+}
+
+int distAtras(){
+  // Trig 12 y Echo 11
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration1 = pulseIn(echoPin, HIGH);
+
+  return duration1 * 0.034 / 2;
+}
+int distFrente(){
+  // Trig 3 y echo 2
+  digitalWrite(trigPin2, LOW);
+  delayMicroseconds(2);
+  
+  digitalWrite(trigPin2, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin2, LOW);
+  duration2 = pulseIn(echoPin2, HIGH);
+
+  return duration2 * 0.034 / 2;
+}
+
+void loop() {
+  recibir();
+  distance1 = distFrente();
+  distance2 = distAtras();
+  Serial.println(estado);
+      
+  if (mensaje_disp()){
+    String aux = leerMensaje();
     
+    if (aux[1] == inGrua1 && estado == CARGA){estado = ADELANTE;} 
+        
+    if (aux[2] == inGrua2 && estado == DESCARGA){estado = ATRAS;}
 
+    if (aux[3] == Desactivar){estado = DESACTIVADO;}
+      
+    aux = "";}
+
+  if (distance1 < 5 && estado == ADELANTE){
+    estado = DESCARGA;
+    //llegue enviar mensaje Grua2
+    BT.write(outGrua2);
+    }
+
+  if (distance2 < 5 && estado == ATRAS){
+    estado = CARGA;
+    //llegue enviar mensaje Grua1
+    BT.write(outGrua1);
   }
- 
-  if(Serial.available())  // Aca podemos controlar lo que se manda al bluetooh
-  {
-     BT.write(Serial.read());
+  if (distance1 == 0 || distance1 > 250 || distance2 == 0 || distance2 > 250 ){
+    desviado = ACTIVADO;
   }
+  if (desviado == ACTIVADO && distance1 != 0 && distance2 != 0 && distance1<250 && distance2<250)  { desviado = DESACTIVADO;}
+
+
+  if (estado == ADELANTE){
+      myMotor1->run(FORWARD);
+      myMotor2->run(FORWARD);}
+
+  if (estado == ATRAS){
+      myMotor1->run(BACKWARD);
+      myMotor2->run(BACKWARD);}
+  
+  if (estado == CARGA || estado == DESCARGA){
+    myMotor1->run(RELEASE);
+    myMotor2->run(RELEASE); }    
+    
+  if (desviado == ACTIVADO){
+    myMotor1->run(RELEASE);
+    myMotor2->run(RELEASE);
+  }
+  if (estado == DESACTIVADO){
+    myMotor1->run(RELEASE);
+    myMotor2->run(RELEASE);
+  }
+
+  if (desviado == DESACTIVADO && estado == ADELANTE){
+      myMotor1->run(FORWARD);
+      myMotor2->run(FORWARD);}
+      
+  if (desviado == DESACTIVADO && estado == ATRAS){
+    myMotor1->run(BACKWARD);
+    myMotor2->run(BACKWARD);}
+
 }
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
+import time
+
 import utilx
 import serial
-import time
 import platform
 import threading
-
 
 def greeting():
     print('''
@@ -17,67 +17,83 @@ O ingresar correctamente los COM que vas a utilizar si estas en modo debug.
     input("Una vez preparado, Presiona cualquier tecla para continuar")
     return True
 
-
-debug=True
-
-
-
-
+debug = True
 
 if debug and platform.system() == 'Darwin' or debug and platform.system() == 'Linux':
-    puertos = ['/dev/cu.ArduinoMasterBluetooh']
+    puertos = ['/dev/tty.R2D2'] #aca se anota los puertos si se utilizada linux o mac
+
 elif debug and platform.system() == 'Windows':
-    puertos = ['COM4']
+    puertos = ['COM4'] #aca de agrega los puertos en windows,
 else:
-    puertos=utilx.serial_ports() #toma todos los puertos com conectados sin distincion
+    puertos = utilx.serial_ports()  # toma todos los puertos com conectados sin distincion
 
 continuar = greeting()
 if continuar:
     comunica = []
     for p in puertos:
+        #se abre los puertos y se agrega a la lista comunica
         print("abriendo puerto", p)
         try:
             comunica.append(serial.Serial(port=str(p), baudrate=9600, timeout=0.5, write_timeout=0))
         except Exception as e:
             print("Error al abrir puerto", e)
 
-def recibido(puerto,numero_puerto):
+
+def recibido(puerto, numero_puerto: int):
     "los puertos van desde {0,1,2,3}"
     while True:
         byte = puerto.read()
-        if len(byte)>0:
+        if len(byte) > 0:
             strings = "".join(map(chr, byte))
-            print(strings)
-            if strings=='1':
+            if strings:
+                #esto se agrega debido a que ocurre un error de parseo de python en los corchetes
+                strings = strings.replace("\n", "")
+                strings = strings.replace(" ", "")
                 for p in comunica:
-                    if numero_puerto==0:
-                        p.write("{1,0,0,0}".encode('utf-8'))
-                    elif numero_puerto==1:
-                        p.write("{0,1,0,0}".encode('utf-8'))
-                    elif numero_puerto==2:
-                        p.write("{0,0,1,0}".encode('utf-8'))
-                    elif numero_puerto==3:
-                        p.write("{0,0,0,1}".encode('utf-8'))
+                    #aca se juega con la posicion de los puertos, por lo cual es importante tener la identificacion,
+                    #estos van por indice de la lista, des decir comunica[0] es el puerto 0
+
+                    if numero_puerto == 0:
+                        cadena = "{{{0}000}}".format(strings)
+
+                        print(f"recibi {cadena} del puerto 0")
+                        p.write(cadena.encode('utf-8'))
+                    elif numero_puerto == 1:
+                        cadena = "{{0{0}00}}".format(strings)
+                        p.write(cadena.encode('utf-8'))
+                    elif numero_puerto == 2:
+                        cadena = "{{00{0}0}}".format(strings)
+                        p.write(cadena.encode('utf-8'))
+                    elif numero_puerto == 3:
+                        cadena = "{{000{0}}}".format(strings)
+                        p.write(cadena.encode('utf-8'))
 
 
+
+def iniciar_autos(puerto):
+        cadena="{00c0}"
+        puerto.write(cadena.encode('utf-8'))
+        print(f'{cadena} enviado')
 
 
 def escritura(puerto):
     while True:
+        dato="f"
+        #cadena = "{{{0},{0},{0},{0}}}".format(dato)
+        cadena="{aaaa}"
+        puerto.write(cadena.encode('utf-8'))
+        print(f'{cadena} enviado')
         time.sleep(3)
-        puerto.write("1".encode('utf-8'))
-        print("1 enviado")
+        cadena = "{rrrr}"
+        puerto.write(cadena.encode('utf-8'))
+        print(f'{cadena} enviado')
 
-
-
-if __name__=='__main__':
-
+if __name__ =='__main__':
     for p in comunica:
-        t1 = threading.Thread(target=recibido, args=(p,comunica.index(p)))
+        #por cada puerto abierto agregado a la lista comunica se crea un thread con su posicion
+        t1 = threading.Thread(target=recibido, args=(p, comunica.index(p)))
         #t2 = threading.Thread(target=escritura, args=(p,))
+        t3 = threading.Thread(target=iniciar_autos, args=(p,))
         t1.start()
         #t2.start()
-
-
-
-
+        t3.start()
